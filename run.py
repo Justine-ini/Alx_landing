00a62b0e910f07eb9d2d47b5d10cb0013e3a698d
Justine-ini <106019828+Justine-ini@users.noreply.github.com>
2024-02-21 00:56:32 +0100

    Update run.py
    Added comments to the code

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
+# Importing the Flask app instance from the quikvote package
 from quikvote import app
 
+# This conditional block ensures that the app is only run if this script is executed directly,
+# not when it's imported as a module in another script.
 if __name__ == "__main__":
+    # Running the Flask app on the specified host (0.0.0.0) and enabling debug mode for development
     app.run(host="0.0.0.0", debug=True)
